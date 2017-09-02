3ad7a25561e554d2fcee0c365c66934862822162
achmad.aris30@gmail.com <aris300312>
2017-09-02 23:26:17 +0700

    fix prepare

--- app/src/main/java/com/dtech/ytbsearch/Main2Activity.java
+++ app/src/main/java/com/dtech/ytbsearch/Main2Activity.java
@@ -6,24 +6,54 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.GridView;
+
+import com.dtech.ytbsearch.adapter.GridMenu;
 
 public class Main2Activity extends AppCompatActivity {
+
+    public static String[] gridTitle = {
+            "Nella Kharisma",
+            "Via Vallen"
+    };
+
+    public static String[] gridQuery = {
+            "nella+kharisma",
+            "via+vallen"
+    };
+
+    public static int[] gridImage = {
+            R.drawable.vallen,
+            R.drawable.vallen
+    };
+
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+        initUi();
+    }
+
+    private void initUi() {
+        gridView = (GridView) findViewById(R.id.grid);
+
+        GridMenu adapter = new GridMenu(this, gridTitle, gridQuery, gridImage);
+        gridView.setAdapter(adapter);
+
     }
 
 }
---
+++ app/src/main/java/com/dtech/ytbsearch/adapter/GridMenu.java
@@ -0,0 +1,72 @@
+package com.dtech.ytbsearch.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dtech.ytbsearch.R;
+import com.pkmmte.view.CircularImageView;
+
+/**
+ * Created by lenovo on 02/09/2017.
+ */
+
+public class GridMenu extends BaseAdapter {
+
+    Context context;
+    private final String[] title;
+    private final String[] query;
+    private final int[] image;
+
+    public GridMenu(Context context, String[] title, String[] query, int[] image) {
+        this.context = context;
+        this.title = title;
+        this.query = query;
+        this.image = image;
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View grid;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (view == null) {
+            grid = new View(context);
+            grid = inflater.inflate(R.layout.item_grid, null);
+            TextView tTitle = (TextView) grid.findViewById(R.id.tTitle);
+            TextView tQuery = (TextView) grid.findViewById(R.id.tQuery);
+            CircularImageView img = (CircularImageView) grid.findViewById(R.id.imgGrid);
+            img.setBorderColor(R.color.grey);
+            img.setBorderWidth(4);
+            img.addShadow();
+            img.setSelectorStrokeWidth(10);
+
+            tTitle.setText(title[i]);
+            tQuery.setText(query[i]);
+            img.setImageResource(image[i]);
+            /*img.setBackgroundResource(image[i]);*/
+            /*ImageView img = (ImageView)grid.findViewById()*/
+        } else {
+            grid = view;
+        }
+        return grid;
+    }
+}
