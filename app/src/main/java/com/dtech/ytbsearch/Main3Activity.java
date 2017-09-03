50d77198b390c80522405639aba21e21e79d0654
Christiana Fransisca <christianafransisca1827@gmail.com>
2017-09-04 06:44:43 +0700

    prepare fix

--- app/src/main/java/com/dtech/ytbsearch/Main3Activity.java
+++ app/src/main/java/com/dtech/ytbsearch/Main3Activity.java
@@ -7,7 +7,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dtech.ytbsearch.adapter.GridMainMenu;
@@ -36,6 +39,7 @@ public class Main3Activity extends YouTubeBaseActivity implements YouTubePlayer.
 
     private static final int RECOVERY_REQUEST = 1;
     GridView grid;
+    Button btnArtis;
     YouTubePlayerView youTubePlayerView;
 
     @Override
@@ -65,8 +69,29 @@ public class Main3Activity extends YouTubeBaseActivity implements YouTubePlayer.
     private void initUi() {
 
         grid = (GridView) findViewById(R.id.griMain);
+        btnArtis = (Button) findViewById(R.id.bmain);
         GridMainMenu adapter = new GridMainMenu(this, mainTitle, mainQuery);
         grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView ttitle = (TextView) grid.findViewById(R.id.tmain1);
+                TextView vidId = (TextView) grid.findViewById(R.id.tmain2);
+
+                Intent intent = new Intent(Main3Activity.this, PlayActivity.class);
+                intent.putExtra("title", ttitle.getText().toString());
+                intent.putExtra("id", vidId.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        btnArtis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main3Activity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
